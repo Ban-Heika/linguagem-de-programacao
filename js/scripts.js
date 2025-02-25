193a8584c6ceb004ad44dab697c780aab318314d
Ban_Heika <gokucraft2891028@gmail.com>
2025-02-25 09:01:53 -0300

    linguagem-de-programacao

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 function seguir(){
 alert(`você agora está seguindo GordinSalvador`)
 }
+//exercicio 1
+function exe1(){
+    //vamos usar DOM para recuperar dados do html
+    //dom = document object model
+    //converter texto para numero
+    let nro1 = Number(document.getElementById("nro1").value)
+    let nro2 = Number(document.getElementById("nro2").value)
+    //vamos fazer a subtração
+    let sub = nro1 - nro2
+    //vamos mostrar o resultado da subtração
+    //alert("A subtração é " + sub)
+document.getElementById("sub").innerText = "Resultado: " + sub
+}
